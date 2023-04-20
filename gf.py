9789f168b7de81025f45ec38095c8ae9e89499b6
ITZ-JISAN <127544614+X1X4D-2-0@users.noreply.github.com>
2023-04-21 03:27:56 +0600

    Gf
    Hack

---
+++ gf.py
@@ -0,0 +1,259 @@
+# (It'z Jisan Xhowdhury)
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import os
+import random
+import requests,bs4,json,sys,random,datetime,time,re,subprocess,platform,struct
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import base64
+import os,sys,time,json,random,re,string,platform,base64
+import requests
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+import mechanize
+from requests.exceptions import ConnectionError
+import string
+try:
+    import requests
+except ImportError:
+    print('\n [âœ“] installing requests !...\n')
+    os.system('pip install requests')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n [âœ“] installing futures !...\n')
+    os.system('pip install futures')
+try:
+    import bs4
+except ImportError:
+    print('\n [âœ“] installing bs4 !...\n')
+    os.system('pip install bs4')
+R = '\033[1;31m' # PUTIH
+G = '\033[1;32m' # PUTIH
+H = '\033[1;32m' # PUTIH
+Y = '\033[1;33m' # PUTIH
+Q = '\033[1;37m'
+T = '\033[1;34m'
+import requests, os, re, bs4,platform, sys, json, time, random, datetime, subprocess, threading, itertools,base64,uuid,zlib
+from concurrent.futures import ThreadPoolExecutor as ahmadAXI
+from datetime import datetime
+from bs4 import BeautifulSoup
+R = '\x1b[1;91m' 
+G = '\x1b[1;92m' 
+Y = '\x1b[1;93m' 
+### WARNA RANDOM ###
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH
+H = '\x1b[1;92m' # HIJAU
+K = '\x1b[1;93m' # KUNING
+B = '\x1b[1;94m' # BIRU
+U = '\x1b[1;95m' # UNGU
+MXD = '\033[1;92m' #MAHADIGREEN
+O = '\x1b[1;96m' # BIRU MUDA
+N = '\x1b[0m'    # WARNA MATI
+A = '\x1b[1;90m' # WARNA ABU ABU
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+ct = datetime.now()
+n = ct.month
+bulan = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Agustus', 'September', 'October', 'November', 'December']
+try:
+    if n < 0 or n > 12:
+        exit()
+    nTemp = n - 1
+except ValueError:
+    exit()
+def banner():
+    print("[=] ðŸ˜Œ Testing New mathod By hk")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+op = bulan[nTemp]
+P = '\x1b[1;97m' # 
+M = '\033[1;31m' # 
+H = '\033[1;32m' # 
+K = '\x1b[1;97m' # 
+B = '\x1b[1;97m' # 
+U = '\x1b[1;95m' # 
+O = '\x1b[1;97m' # 
+N = '\x1b[0m'    # 
+my_color = [
+ P, M, H, K, B, U, O, N]
+warna = random.choice(my_color)
+data,data2={},{}
+aman,cp,salah=0,0,0
+ubahP,fuck,pwBaru=[],[],[]
+ok = []
+mnum=[]
+cp = []
+id = []
+user = []
+loop = 0
+oks = []
+cps = []
+loop = 0
+url_lookup = "https://lookup-id.com/"
+url_mb = "https://m.facebook.com"
+url_ip = "https://www.httpbin.org/ip"
+header_grup = {"user-agent": "Mozilla/5.0 (Linux; Android 12; Nokia XR20) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"}
+bulan_ttl = {"01": "January", "02": "February", "03": "March", "04": "April", "05": "May", "06": "June", "07": "July", "08": "Augustus", "09": "September", "10": "October", "11": "November", "12": "December"}
+done = False
+ugen=[]
+uas=[]
+usa = ["Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}"]
+rr = random.randint
+for xd in range(3005):
+    ff=(f'Mozilla/5.0 Macintosh; Intel Mac OS X 10_10_2) AppleWebKit/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))} (KHTML, like Gecko) Version/{str(rr(20,100))}.0.{str(rr(1111,9999))} Safari/{str(rr(1111,9999))}.{str(rr(20,100))}.{str(rr(20,100))}')
+    uas.append(ff)
+for JISAN in range(9993,176281):
+    aa='Mozilla/5.0 (Linux; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c='TRT-L53 Build/HUAWEITRT-L53; wv)'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Chrome/69.0.3497.100 Mobile Safari/537.36 [FB_IAB/FB4A;FBAV/192.0.0.34.85;]'
+    uaku2=f'{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+def jalan(z):
+    for e in z + '\n':
+        sys.stdout.write(e)
+        sys.stdout.flush()
+        time.sleep(0.01)
+def menu_apikey():
+  uuid = str(os.getpid()) + str(os.getlogin())
+  id = "→".join(uuid)
+  server = requests.get('https://raw.githubusercontent.com/Hridoykhan104/game.py/main/game.txt').text
+  
+ 
+
+  os.system(" clear")                          
+  print("""\x1b[1;97m
+    _____  ______   ______    ______   __    __       
+   /     |/      | /      \  /      \ /  \  /  |      
+   $$$$$ |$$$$$$/ /$$$$$$  |/$$$$$$  |$$  \ $$ |      
+      $$ |  $$ |  $$ \__$$/ $$ |__$$ |$$$  \$$ |      
+ __   $$ |  $$ |  $$      \ $$    $$ |$$$$  $$ |      
+/  |  $$ |  $$ |   $$$$$$  |$$$$$$$$ |$$ $$ $$ |      
+$$ \__$$ | _$$ |_ /  \__$$ |$$ |  $$ |$$ |$$$$ |      
+$$    $$/ / $$   |$$    $$/ $$ |  $$ |$$ | $$$ |      
+ $$$$$$/  $$$$$$/  $$$$$$/  $$/   $$/ $$/   $$/       
+    ┌───────────────────────────────────────┐
+    │ [✓] AUTHOR   : It'z Jisan Xhowdhury            
+    │ [✓] GITHUB   : Itz-jisan            
+    │ [✓] WHATSAPP : +8801814649133             
+    │ [✓] VERSION  : 1.7.8                                    
+    │ [✓] TOOLS    : \033[1;32m GF / BF HACK  \033[1;37m│
+    └───────────────────────────────────────┘""")
+  print(f"\033[1;32m FIRST GET APPROVEL")
+  print(f"")
+  print(f" \033[1;32m  THIS TOOLS IS PAID SO YOU NEED GET APPROVED FIRST\033[1;37m\n")
+  print(f"")
+  print(f"\x1b[1;92m   contract Admin to Buy this Tools                                                               ");time.sleep (0.1) 
+  print(f"")
+  print(f"\033[1;32     YOUR  KEY : "+id)
+  print(f"")
+  print(f"\033[1;31m   COPY YOUR KEY AND SEND TO ADMIN  ");time.sleep(0.1)
+  print(f"")
+  print(f"  Follow Admin Facebook ID,,,,,,,,,,,,,,,,,    ");time.sleep(1)
+  os.system(f'xdg-open https://www.facebook.com/TurRealabbu1')
+  print(f"");time.sleep(2)
+  print(f"\x1b[0;34m  CHECKING YOUR APROVAL.............                                                ");time.sleep (0.5)
+  
+logo=(f"""\x1b[1;97m
+    _____  ______   ______    ______   __    __       
+   /     |/      | /      \  /      \ /  \  /  |      
+   $$$$$ |$$$$$$/ /$$$$$$  |/$$$$$$  |$$  \ $$ |      
+      $$ |  $$ |  $$ \__$$/ $$ |__$$ |$$$  \$$ |      
+ __   $$ |  $$ |  $$      \ $$    $$ |$$$$  $$ |      
+/  |  $$ |  $$ |   $$$$$$  |$$$$$$$$ |$$ $$ $$ |      
+$$ \__$$ | _$$ |_ /  \__$$ |$$ |  $$ |$$ |$$$$ |      
+$$    $$/ / $$   |$$    $$/ $$ |  $$ |$$ | $$$ |      
+ $$$$$$/  $$$$$$/  $$$$$$/  $$/   $$/ $$/   $$/       
+    ┌───────────────────────────────────────┐
+    │ [✓] AUTHOR   : It'z Jisan Xhowdhury         
+    │ [✓] GITHUB   : Itz-jisan          
+    │ [✓] WHATSAPP : +8801814649133      
+    │ [✓] VERSION  : 1.7.8                                 
+    │ [✓] TOOLS    : \033[1;32m GF / BF HACK  \033[1;37m│
+    └───────────────────────────────────────┘""")
+def cek_apk(session,coki):
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO ACTIVE APK")
+	else:
+		print("")
+		print(f'\r🎮 %sYOUR ACTIVE APPLICATION DETAILS :'%(H))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(H,i+1,game[i].replace("ACTIVE"," ACTIVE"),N))
+	w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+	sop = BeautifulSoup(w,"html.parser")
+	x = sop.find("form",method="post")
+	game = [i.text for i in x.find_all("h3")]
+	if len(game)==0:
+		print(f"\r{N}[{M}!{N}] SORRY THERE IS NO EXPIRED APK")
+	else:
+		print(f'\r 🎮 %sYOUR EXPIRED APPLICATION DETAILS :'%(M))
+		for i in range(len(game)):
+			print("%s%s. %s%s"%(K,i+1,game[i].replace("Expired"," Expired"),N))
+def Main():
+	os.system('clear')
+	print(logo)
+	print(50*'_')
+	print(' [1] GF HACK ')
+	print(' [2] BF HACK')
+	print(' [0] Back to Main menu')
+	bal = input('Choose option >>> ')
+	if bal =='1':
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100090358010883');time.sleep(1)
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100088525610543');time.sleep(1)
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100081923926358');time.sleep(1)
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100080251227514');time.sleep(1)
+		os.system('xdg-open https://www.facebook.com/profile.php?id=100088723881886');time.sleep(1)
+ ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️   ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️  ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️Congratulations  admin()
+	if bal =='2':
+		os.system('xdg-open https://www.facebook.com/TurRealabbu1');time.sleep(1)
+		admin()
+	if bal =='0':
+		Main()
+	
+def virus():
+	user=[]
+	os.system('clear')
+	print(logo)
+	print(" Congratulations ")
+		os.system('clear')
+		print(logo)
+		tl = str(len(user))
+		print(f"\033[1;97m [🔥]  version:\033[1;92m {doamin}")
+		print(' \033[1;97m[🔥]  The process has been started')
+		print(' [🔥]  Congratulations YOUR GF ID https://www.facebook.com/profile.php?id=100080251227514 ')
+		print(' [🔥]  Congratulations YOUR GF ID https://www.facebook.com/profile.php?id=100088723881886 ')
+		print(' [🔥]  Congratulations YOUR GF ID https://www.facebook.com/profile.php?id=100088525610543 ')
+		print(50*'_')
+	 ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️  print(' [🔥] Crack process has been completed')
+	 ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️  print(' [🔥] Ids saved in ok.txt,cp.txt')
+	 ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️  print(50*'_') ️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️️
+                break
+            else:
+                continue
+        loop+=1        
+    except:
+        pass
+Main()
